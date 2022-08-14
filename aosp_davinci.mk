50705ed60a1de05fab746d24fe4df42b7d7aaeb4
ad1tyas1ngh <adityasingh1768@gmail.com>
2022-08-15 05:22:47 +0530

    davinci: AOSP-ify
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -11,13 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := aosp_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 9T
+PRODUCT_MODEL := Redmi K20
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
